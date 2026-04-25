ef767e8b7f7726a426f249bb2a70a5a17ef751d6
DoxlieOwO <139929735+DoxlieOwO@users.noreply.github.com>
2026-04-25 12:44:01 -0400

    Refactor key input handling for Character context
    Updated KeyDown and KeyUp events to use Character. Added functions to handle key input events.

--- main.lua
+++ main.lua
@@ -13,24 +13,59 @@ RemoteFolder.Name = "Remotes"
 
 
 --------------------- [[ HEADER ]] ---------------
-local KeyDown = Instance.new("RemoteEvent", RemoteFolder)
+local KeyDown = Instance.new("RemoteEvent", Character)
 KeyDown.Name = "KeyDown"
 
-local KeyUp = Instance.new("RemoteEvent", RemoteFolder)
+local KeyUp = Instance.new("RemoteEvent", Character)
 KeyUp.Name = "KeyDown"
 
 
 --------------------- [[ FUNCTIONS ]] ---------------
 
+local function OnKeyDown(Input, GPE)
+    if GPE then return end
 
+    print("On Key Down", Input.KeyCode)
+end
+
+local function OnKeyUp(Input, GPE)
+    if GPE then return end
+
+    print("On Key Up", Input.KeyCode)
+end
 
 
 --------------------- [[ INIT ]] ---------------
 
 NLS([[
-local Services = {}    
+local KeyUp = script.Parent:WaitForChild("KeyUp")
+local KeyDown = script.Parent:WaitForChild("KeyDown")
+
+local UserInputService = game:GetService("UserInputService")
+local Mouse = owner:GetMouse() :: Mouse
+
+
+
+
+UserInputService.InputBegan:Connect(function(input, gpe)
+	KeyUp:FireServer({UserInputState = input.UserInputState, KeyCode = input.KeyCode, UserInputType = input.UserInputType}, gpe)
+end)
+
+UserInputService.InputEnded:Connect(function(input, gpe)
+	KeyDown:FireServer({UserInputState = input.UserInputState, KeyCode = input.KeyCode, UserInputType = input.UserInputType}, gpe)
+end) 
 ]], Character)
 
 
 --------------------- [[ MAIN ]] ---------------
+KeyDown.OnServerEvent:Connect(funcion(iPlayer, Input, GPE)
+    if iPlayer ~= Player then return end
+    OnKeyDown(Input, GPE)
+end)
+
+KeyUp.OnServerEvent:Connect(funcion(iPlayer, Input, GPE)
+    if iPlayer ~= Player then return end
+    OnKeyUp(Input, GPE)
+end)
+
 print("COLEO realchar base loaded: ", Player)
